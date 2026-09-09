0cb6d8a584e0d20b99f2cc41d361018294aec974
N <i.naveen.prakash@gmail.com>
2026-09-09 12:54:44 +0530

    pre1 180.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         minSdk = 30
         //noinspection OldTargetApi
         targetSdk = 36
-        versionCode = 179
-        versionName = "179.0"
+        versionCode = 180
+        versionName = "180.0"
 
         renderscriptTargetApi = 18
         renderscriptSupportModeEnabled = true
